8cbffb52147745246618c0b832c2020436f68a2c
Steve Anderson <steve@fuzzwork.co.uk>
2017-03-06 05:12:22 +0100

    compressed ore table

---
+++ table.sql
@@ -0,0 +1,14 @@
+CREATE TABLE `compressedOre` (
+      `id` bigint(21) DEFAULT NULL,
+      `typeid` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `compressedOre`
+--
+
+LOCK TABLES `compressedOre` WRITE;
+/*!40000 ALTER TABLE `compressedOre` DISABLE KEYS */;
+INSERT INTO `compressedOre` VALUES (1,28367),(2,28388),(3,28391),(4,28397),(5,28401),(6,28403),(7,28406),(8,28410),(9,28413),(10,28416),(11,28420),(12,28422),(13,28424),(14,28429),(15,28432);
+
